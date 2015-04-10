b2d07f8589466264c40a471d8c2c51a0ef1a94c7
Julio Lopez <ljuliom@gmail.com>
2015-04-10 11:25:06 -0500

    starting tests

---
+++ test/test_fabricas.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../lib/fabricas", File.dirname(__FILE__))
+
+class User
+  attr_accessor :name, :email
+end
+
+Fabricas.define do
+  factory :user do
+    name "Julio"
+    email "email@gmail.com"
+  end
+end
+
+scope do
+  test "build user returns name and email" do
+    user = Fabricas.build :user
+    assert_equal user.name, "Julio"
+    assert_equal user.email, "email@gmail.com"
+  end
+
+  test "build user with name returns the sent name" do
+    user = Fabricas.build :user, name: "Pedro"
+    assert_equal user.name, "Pedro"
+    assert_equal user.email, "email@gmail.com"
+  end
+end
