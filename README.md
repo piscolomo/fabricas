08bdf4f75420eb4ecab8f172f4ca70e2cca14cb0
Julio Lopez <ljuliom@gmail.com>
2015-04-10 11:13:13 -0500

    first functional fabricas

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# fabricas
+Fabricas
+====
+
 Another minimalist library for build factories
+
+Usage
+-----
+
+```ruby
+class User
+  attr_accessor :name, :email
+end
+
+Fabricas.define do
+  factory :user do
+    name "Julio"
+    email "email@gmail.com"
+  end
+end
+
+user = Fabricas.build :user
+puts user.inspect
+#=> <User @name= "Julio", @email= "email@gmail.com">
+
+other_user = Fabricas.build :user, name: "Pedro"
+puts other_user.inspect
+#=> <User @name= "Pedro", @email= "email@gmail.com">
+```
---
+++ lib/fabricas.rb
@@ -0,0 +1,37 @@
+module Fabricas
+  @items = {}
+  def self.items; @items; end
+
+  def self.define(&block)
+    FabricasMethods.new.instance_eval(&block)
+  end
+
+  def self.build(klass, values = {})
+    _i = const_get(klass.capitalize).new
+    attributes = items[klass].attributes.merge(values)
+    attributes.each do |name, value|
+      _i.send("#{name}=", value)
+    end
+    _i
+  end
+
+  class CleanRoom < BasicObject
+    attr_reader :attributes
+
+    def initialize
+      @attributes = {}
+    end
+
+    def method_missing(method_name, *args)
+      attributes[method_name] = args.first
+    end
+  end
+
+  class FabricasMethods
+    def factory(klass, &block)
+      @class_name = Module.const_get(klass.capitalize)
+      Fabricas.items[klass] = CleanRoom.new if block_given?
+      Fabricas.items[klass].instance_eval(&block)
+    end
+  end
+end
